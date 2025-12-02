fb267c49e050643ebd5970514feddfd5ec8ef2ba
Freddy Arevalo <delavega3540@gmail.com>
2025-12-01 21:15:50 -0600

    Refactor: Simplify Table component syntax while preserving styles

--- src/components/ui/table.jsx
+++ src/components/ui/table.jsx
@@ -1,82 +1,38 @@
-import * as React from "react"
-import { cn } from "@/lib/utils"
+import React from 'react';
+import { cn } from "@/lib/utils";
 
-const Table = React.forwardRef(({ className, ...props }, ref) => (
-    <div className="relative w-full overflow-auto">
-        <table
-            ref={ref}
-            className={cn("w-full caption-bottom text-sm", className)}
-            {...props} />
-    </div>
-))
-Table.displayName = "Table"
+export function Table({ className, ...props }) {
+    return (
+        <div className="relative w-full overflow-auto">
+            <table className={cn("w-full caption-bottom text-sm", className)} {...props} />
+        </div>
+    );
+}
 
-const TableHeader = React.forwardRef(({ className, ...props }, ref) => (
-    <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
-))
-TableHeader.displayName = "TableHeader"
+export function TableHeader({ className, ...props }) {
+    return <thead className={cn("[&_tr]:border-b", className)} {...props} />;
+}
 
-const TableBody = React.forwardRef(({ className, ...props }, ref) => (
-    <tbody
-        ref={ref}
-        className={cn("[&_tr:last-child]:border-0", className)}
-        {...props} />
-))
-TableBody.displayName = "TableBody"
+export function TableBody({ className, ...props }) {
+    return <tbody className={cn("[&_tr:last-child]:border-0", className)} {...props} />;
+}
 
-const TableFooter = React.forwardRef(({ className, ...props }, ref) => (
-    <tfoot
-        ref={ref}
-        className={cn("border-t bg-muted/50 font-medium [&>tr]:last:border-b-0", className)}
-        {...props} />
-))
-TableFooter.displayName = "TableFooter"
+export function TableFooter({ className, ...props }) {
+    return <tfoot className={cn("border-t bg-muted/50 font-medium [&>tr]:last:border-b-0", className)} {...props} />;
+}
 
-const TableRow = React.forwardRef(({ className, ...props }, ref) => (
-    <tr
-        ref={ref}
-        className={cn(
-            "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
-            className
-        )}
-        {...props} />
-))
-TableRow.displayName = "TableRow"
+export function TableRow({ className, ...props }) {
+    return <tr className={cn("border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted", className)} {...props} />;
+}
 
-const TableHead = React.forwardRef(({ className, ...props }, ref) => (
-    <th
-        ref={ref}
-        className={cn(
-            "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
-            className
-        )}
-        {...props} />
-))
-TableHead.displayName = "TableHead"
+export function TableHead({ className, ...props }) {
+    return <th className={cn("h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0", className)} {...props} />;
+}
 
-const TableCell = React.forwardRef(({ className, ...props }, ref) => (
-    <td
-        ref={ref}
-        className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
-        {...props} />
-))
-TableCell.displayName = "TableCell"
+export function TableCell({ className, ...props }) {
+    return <td className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)} {...props} />;
+}
 
-const TableCaption = React.forwardRef(({ className, ...props }, ref) => (
-    <caption
-        ref={ref}
-        className={cn("mt-4 text-sm text-muted-foreground", className)}
-        {...props} />
-))
-TableCaption.displayName = "TableCaption"
-
-export {
-    Table,
-    TableHeader,
-    TableBody,
-    TableFooter,
-    TableHead,
-    TableRow,
-    TableCell,
-    TableCaption,
+export function TableCaption({ className, ...props }) {
+    return <caption className={cn("mt-4 text-sm text-muted-foreground", className)} {...props} />;
 }
